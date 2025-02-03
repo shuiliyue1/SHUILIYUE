ec111e8d3a31406e3e9dca6ed237973759b4c862
shuiliyue1 <shuiliyue1@outlook.com>
2025-02-03 23:39:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '电影', url: '/tag/电影' },
   HEO_HERO_CATEGORY_2: { title: '剧集', url: '/tag/剧集' },
-  HEO_HERO_CATEGORY_3: { title: '合集', url: '/tag/合集' },
+  HEO_HERO_CATEGORY_3: { title: '', url: '/tag/' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
